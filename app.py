80c9c99316fa5b1bee242ae9a7b9a8e7adc0c0b7
Eric Button <eric.button@protonmail.com>
2022-08-26 10:31:18 -0500

    8.26 Updates

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine,reflect=True)
+
+Measurement = Base.classes.measurement
+
+Station = Base.classes.station
 
 app = Flask(__name__)
 
@@ -13,6 +30,26 @@ def welcome():
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end><br/>"
     )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    results = session.query(Measurement.date,Measurement.prcp)
+
+    session.close()
+
+
+    prcp_data = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict['date'] = date
+        prcp_dict['prcp'] = prcp
+        prcp_data.append(prcp_dict)
+    print(prcp_dict)
+
+
+    return jsonify(prcp_data)
 
 
 
